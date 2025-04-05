73dde5185e11b04ddd5653d650f9e7f1e8e46331
WWamich <admiral3233@mail.ru>
2025-04-05 20:17:38 +0300

    added .gitignore

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/admir/CLionProjects/untitled7
+# Install script for directory: /mnt/c/Users/admir/CLionProjects/asynciC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/admir/CLionProjects/untitled7/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Users/admir/CLionProjects/asynciC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
